c349e57a48e6319c8c693d2a34b9a30b627e766d
CC Katz <cckatz@CCs-MacBook-Pro.fios-router.home>
2018-09-11 15:50:31 -0400

    initial commit

---
+++ TheraConnect/User.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  TheraConnect
+//
+//  Created by CC Katz on 8/30/18.
+//  Copyright © 2018 CC Katz. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    let name: String
+    let email: String
+    let username: String
+    let type: User
+    init(name: String, email: String, username: String, type: User) {
+        self.name = name
+        self.email = email
+        self.username = username
+        self.type = type
+    }
+}
+
+class Patient: User {
+    
+}
+class Therapist: User {
+    
+}
